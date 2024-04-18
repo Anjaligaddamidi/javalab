838a6b4eef56fd7b22e6fd0034957079120b51c1
Anjaligaddamidi <149222107+Anjaligaddamidi@users.noreply.github.com>
2024-04-18 13:11:23 +0530

    Add files via upload
    subtraction of 2 numbers

---
+++ Subtraction.java
@@ -0,0 +1,9 @@
+class Subtraction
+{
+public static void main(String[] args)
+{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+System.out.println("subtraction of"+a+"-"+b+"is"+(a-b));
+}
+}
