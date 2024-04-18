91674c24a23930deb4a17b86acd3299b8e6f74ee
Anjaligaddamidi <149222107+Anjaligaddamidi@users.noreply.github.com>
2024-04-18 13:13:23 +0530

    Add files via upload
    operations on integers

---
+++ Math.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Math
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the values of a and b:");
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println("addition of"+a+"+"+b+"is"+(a+b));
+System.out.println("addition of"+a+"-"+b+"is"+(a-b));
+System.out.println("addition of"+a+"*"+b+"is"+(a*b));
+System.out.println("addition of"+a+"/"+b+"is"+(a/b));
+System.out.println("addition of"+a+"%"+b+"is"+(a%b));
+}
+}
+
