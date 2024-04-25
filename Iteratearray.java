044fecaf403ec7222ccc7d5a8bc4a62e665c0066
Anjaligaddamidi <149222107+Anjaligaddamidi@users.noreply.github.com>
2024-04-25 13:03:38 +0530

    Add files via upload
    program on iteration of array

---
+++ Iteratearray.java
@@ -0,0 +1,14 @@
+class Iteratearray{
+                public static void main(String[] ar){
+                        int[] arr={1,2,4,5,6,7};
+                        ComArray co=new ComArray();
+                        for(int l:co.arr3){
+                                System.out.println(l);
+                        }
+                        ComArray.main(null);
+                        System.out.println("elements are");
+                        for(int i=0;i<arr.length;i++){
+                                System.out.println(arr[i]);
+                        }
+                }
+}
