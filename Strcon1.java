8144c12c156a405e3218cc2fbce2d55c21490b97
Anjaligaddamidi <149222107+Anjaligaddamidi@users.noreply.github.com>
2024-04-25 13:11:51 +0530

    Add files via upload
    program on string comparision

---
+++ Strcon1.java
@@ -0,0 +1,9 @@
+class Strcon1{
+public static void main(String[] args){
+String s1="rahul";
+String s2="rahul";
+String s3=new String("rahul");
+System.out.println(s1==s2);
+System.out.println(s1==s3);
+}
+}
