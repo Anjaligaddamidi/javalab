479634b42ccae44566e8d62a22778f2b8db55b75
Anjaligaddamidi <149222107+Anjaligaddamidi@users.noreply.github.com>
2024-04-25 13:13:59 +0530

    Add files via upload
    program on string replacement

---
+++ StrCon2.java
@@ -0,0 +1,8 @@
+class StrCon2{
+public static void main(String[] args){
+String s1="rahul";
+System.out.println(s1.replace('r','R'));
+System.out.println(s1.toLowerCase());
+System.out.println(s1.toUpperCase());
+}
+}
